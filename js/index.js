0224e676a389338eefce9b286b75c90610d47d6c
bbibbu0223 <sweetbox95@naver.com>
2021-08-19 19:53:11 +0900

    0819(3)

--- js/index.js
+++ js/index.js
@@ -161,5 +161,48 @@ $(function(){
         var position = $(window).scrollTop(); $(".floating_btn").stop().animate({"top":position+currentPosition+"px"},1000); }); 
     });
 
+    function test(imageName) {
+        LoadingWithMask('your site\'s image path');
+        setTimeout("closeLoadingWithMask()", 3000);
+    }
+
+    // //로딩화면
+
+    // function LoadingWithMask(gif) {
+        
+    //     var maskHeight = $(document).height();
+    //     var maskWidth  = window.document.body.clientWidth;
+    
+        
+    //     var mask       = "<div id='mask' style='position:absolute; z-index:9000; background-color:#000000; display:none; left:0; top:0;'></div>";
+    //     var loadingImg = '';
+    
+    //     loadingImg += " <img src='"+ gif + "' style='position: absolute; display: block; margin: 0px auto;'/>";
+
+    //     //화면에 레이어 추가
+    //     $('body')
+    //         .append(mask)
+
+    //     //마스크의 높이와 너비를 화면 것으로 만들어 전체 화면을 채웁니다.
+    //     $('#mask').css({
+    //             'width' : maskWidth,
+    //             'height': maskHeight,
+    //             'opacity' : '0.3'
+    //     }); 
+
+    //     //마스크 표시
+    //     $('#mask').show();
+
+    //     //로딩중 이미지 표시
+    //     $('#loadingImg').append(loadingImg);
+    //     $('#loadingImg').show();
+    // }
+
+    // function closeLoadingWithMask() {
+    //     $('#mask, #loadingImg').hide();
+    //     $('#mask, #loadingImg').empty();  
+    // }
+    
+    
 
 });
